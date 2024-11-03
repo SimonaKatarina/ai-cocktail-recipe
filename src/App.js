1747a7e843d4a54de2cb226a38509ee6ea6bcd42
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-11-03 08:58:36 +0100

    Added Navbar
    CustomNavbar.js = Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import CustomNavbar from './components/CustomNavbar';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from "./Home";
+import Recipes from "./Recipes";
+import Generator from "./Generator";
 import './App.css';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hello</h1>
-      </header>
-    </div>
+    <Router>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/recipes' element={<Recipes />} />
+        <Route path='/generator' element={<Generator />} />
+      </Routes>
+    </Router>
   );
 }
 
